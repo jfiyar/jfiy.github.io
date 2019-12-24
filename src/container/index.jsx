e651bcc248cc3aa43992a4e3b6c4332db847ddf6
jianglikui <jianglikui@mocaapp.cn>
2019-12-24 09:51:13 +0800

    初始化项目

--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,14 +1,11 @@
 import React, { Component } from 'react'
 import './global.less'
-import ShowLoveBanner from './ShowLoveBanner'
-import OurLoveStory from './OurLoveStory'
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <ShowLoveBanner />
-        <OurLoveStory />
+      <div style={{ textAlign: 'center' }}>
+        <h1>hello thymes!</h1>
       </div>
     )
   }

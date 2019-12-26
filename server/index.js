edaa1e0ce07e5c3fbc952fece470dcb310b570bc
jianglikui <jianglikui@mocaapp.cn>
2019-12-26 12:35:38 +0800

    test

--- server/index.js
+++ server/index.js
@@ -12,37 +12,39 @@ async function main() {
 
   console.log(chalk.blue(port, env))
 
-  webpack({
+  const watcher = webpack({
     entry: './src/index.js',
     watch: true,
-    devServer: { hot: true },
+    devServer: { hot: true, port, contentBase: path.join(__dirname, 'dist'), compress: true },
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-  }).watch(
-    {
-      watchOptions: {
-        // 不监听的文件和文件夹，支持正则
-        ignore: /node_modules/,
-        // 监听到变化等待300ms 再去执行文件防止文件更新太快导致编译频繁
-        // 默认300ms
-        aggregateTimeout: 300,
-        // 判断文件是否发生变化是通过不停地询问系统指定文件有没有变化实现的
-        poll: 1000,
-      },
-      // watch:true时有效
-      aggregateTimeout: 300,
-    },
-    (err, stats) => {
-      if (err || stats.hasErrors()) {
-        console.log(stats)
-        console.log('构建过程出错！')
-      } else {
-        console.log('实时构建成功！')
-      }
-    }
-  )
+  })
+  console.log(watcher)
+  // .watch(
+  //   {
+  //     watchOptions: {
+  //       // 不监听的文件和文件夹，支持正则
+  //       ignore: /node_modules/,
+  //       // 监听到变化等待300ms 再去执行文件防止文件更新太快导致编译频繁
+  //       // 默认300ms
+  //       aggregateTimeout: 300,
+  //       // 判断文件是否发生变化是通过不停地询问系统指定文件有没有变化实现的
+  //       poll: 1000,
+  //     },
+  //     // watch:true时有效
+  //     aggregateTimeout: 300,
+  //   },
+  //   (err, stats) => {
+  //     if (err || stats.hasErrors()) {
+  //       console.log(stats)
+  //       console.log('构建过程出错！')
+  //     } else {
+  //       console.log('实时构建成功！')
+  //     }
+  //   }
+  // )
   // .close(() => {
   //   console.log('服务已关闭')
   // })

af0102b31b021d2ec76e450727f149766aa74c7f
jianglikui <jianglikui@mocaapp.cn>
2020-01-09 10:15:08 +0800

    Update 调整启动参数，script组织结构

---
+++ scripts/build.js
@@ -0,0 +1,33 @@
+module.exports = async function() {
+  const chalk = require('chalk')
+  const portfinder = require('portfinder')
+  const WebpackDevServer = require('webpack-dev-server')
+  const webpack = require('webpack')
+  const webpackConfig = require('../config/webpack')
+  const address = require('address')
+
+  portfinder.basePort = webpackConfig.devServer.port
+
+  const hasArgv = name => process.argv.filter(arg => arg === name).length !== 0
+
+  webpackConfig.mode = hasArgv('prod') ? 'production' : 'development'
+  const port = await portfinder.getPortPromise()
+  const compiler = webpack(webpackConfig)
+  webpackConfig.devServer.after = () => {
+    if (typeof webpackConfig.devServer === 'function') {
+      webpackConfig.devServer()
+    }
+    setTimeout(() => {
+      console.log(`${chalk.blue('\ni')}${chalk.blackBright('[scripts]')}server at:`)
+      console.log(
+        chalk.blue(`
+              http://localhost:${port}
+              http://127.0.0.1:${port}
+              http://${address.ip()}:${port}
+          `)
+      )
+    })
+  }
+  const server = new WebpackDevServer(compiler, webpackConfig.devServer)
+  server.listen(port, '0.0.0.0')
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,11 @@
 const chalk = require('chalk')
-const portfinder = require('portfinder')
-const WebpackDevServer = require('webpack-dev-server')
-const webpack = require('webpack')
-const webpackConfig = require('../config/webpack')
-const address = require('address')
 
-portfinder.basePort = webpackConfig.devServer.port
+const hasArgv = name => process.argv.filter(arg => arg === name).length !== 0
 
-const argvHas = name => process.argv.filter(arg => arg === name).length !== 0
-
-webpackConfig.mode = argvHas('prod') ? 'production' : 'development'
-
-const isBuild = argvHas('build')
-
-async function main() {
-  const port = await portfinder.getPortPromise()
-  const compiler = webpack(webpackConfig)
-  webpackConfig.devServer.after = () => {
-    if (typeof webpackConfig.devServer === 'function') {
-      webpackConfig.devServer()
-    }
-    setTimeout(() => {
-      console.log(`${chalk.blue('\ni')}${chalk.blackBright('[scripts]')}server at:`)
-      console.log(
-        chalk.blue(`
-          http://localhost:${port}
-          http://127.0.0.1:${port}
-          http://${address.ip()}:${port}
-      `)
-      )
-    })
-  }
-  const server = new WebpackDevServer(compiler, webpackConfig.devServer)
-  server.listen(port, '0.0.0.0')
+if (hasArgv('build')) {
+  require('./build')()
+} else if (hasArgv('serve')) {
+  require('./serve')()
+} else {
+  console.log(chalk.yellow(process.argv.slice(2).join(' ') + ' 参数无法识别'))
 }
-
-main()

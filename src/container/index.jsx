65ac34aeafdca83497d431b8905ec3e119b3e5c2
jianglikui <jianglikui@mocaapp.cn>
2020-01-10 09:12:08 +0800

    feat api测试页面

--- src/container/index.jsx
+++ src/container/index.jsx
@@ -1,11 +1,124 @@
 import React, { Component } from 'react'
 import './global.less'
+import Axios from 'axios'
 
 export class App extends Component {
+  state = {
+    params: [],
+    url: '',
+    method: 'get',
+    paramType: 'querystring',
+    resp: '',
+  }
   render() {
+    const { params } = this.state
     return (
-      <div style={{ textAlign: 'center' }}>
-        <h1>hello thymes!!</h1>
+      <div>
+        <h1 style={{ textAlign: 'center' }}>cors api tester</h1>
+        <hr />
+        <table style={{ margin: '0 auto' }}>
+          <tbody>
+            <tr>
+              <td>url:</td>
+              <td>
+                <input style={{ width: 200 }} onChange={e => this.setState({ url: e.target.value })} value={this.state.url} type="text" />
+              </td>
+            </tr>
+            <tr>
+              <td>method:</td>
+              <td>
+                <select>
+                  <option value="get">get</option>
+                  <option value="post">post</option>
+                  <option value="put">put</option>
+                  <option value="delete">delete</option>
+                  <option value="head">head</option>
+                  <option value="patch">patch</option>
+                </select>
+              </td>
+            </tr>
+            <tr>
+              <td>paramType: </td>
+              <td>
+                <select>
+                  <option value="querystring">querystring</option>
+                  <option value="formdata">formdata</option>
+                  <option value="body">body</option>
+                </select>
+              </td>
+            </tr>
+            {params.map((p, i) => {
+              return (
+                <tr key={p.key}>
+                  <td>
+                    <button
+                      onClick={() => {
+                        const newParams = [...params]
+                        newParams.splice(i, 1)
+                        this.setState({ params: newParams })
+                      }}
+                    >
+                      x
+                    </button>
+                    <input
+                      placeholder="name"
+                      style={{ width: 80 }}
+                      onChange={e => {
+                        params[i] = { ...params[i] }
+                        params[i].name = e.target.value
+                        this.setState({ params: [...params] })
+                      }}
+                      value={p.name}
+                    />
+                    :
+                  </td>
+                  <td>
+                    <input
+                      placeholder="value"
+                      style={{ width: 200 }}
+                      onChange={e => {
+                        params[i] = { ...params[i] }
+                        params[i].value = e.target.value
+                        this.setState({ params: [...params] })
+                      }}
+                      value={p.value}
+                    />
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        <hr />
+        <button
+          style={{ width: '100%' }}
+          onClick={() => {
+            const newParams = [...params]
+            newParams.push({ key: Math.random(), name: '', value: '' })
+            this.setState({ params: newParams })
+          }}
+        >
+          add param
+        </button>
+        <button
+          onClick={() => {
+            const data = {}
+            params.map(p => {
+              data[p.name] = p.value
+            })
+            Axios({
+              url: this.state.url,
+              method: this.state.method,
+              data: data,
+            }).then(resp => {
+              this.setState({ resp })
+            })
+          }}
+          style={{ width: '100%', marginTop: 10 }}
+        >
+          submit
+        </button>
+        <pre style={{ whiteSpace: 'pre-wrap', fontSize: 12 }}>{JSON.stringify(this.state.resp.data, null, 4)}</pre>
       </div>
     )
   }

b0ebd5fea9354014dfca94c3f06497cb7edfac99
jianglikui <jianglikui@mocaapp.cn>
2020-01-15 09:36:23 +0800

    test

--- src/index.jsx
+++
@@ -1,9 +0,0 @@
-import ReactDom from 'react-dom'
-import React from 'react'
-import { App } from './container'
-
-const rootElement = document.createElement('div')
-
-document.body.appendChild(rootElement)
-
-ReactDom.render(<App />, rootElement)

01f814fa58c84bd439df59c5097519be76ccf142
jianglikui <jianglikui@mocaapp.cn>
2020-02-08 14:36:43 +0800

    init server

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,9 @@ export interface Props {
 }
 
 export default ({ name }: Props) => {
-  const [count, setCount] = useState(0);
-  const [countLoading, setCountLoading] = useState(false);
-  useEffect(() => {
-    if (!countLoading) {
-      setTimeout(() => {
-        setCountLoading(true);
-        setCount(10);
-      }, 300);
-    }
-  }, [countLoading]);
   return (
     <div style={{ textAlign: "center" }}>
       <h1>hello {name}!</h1>
-      <h2>{countLoading ? count : "loading"}</h2>
-      <h2>
-        {countLoading && (
-          <button onClick={() => setCount(count + 1)}>+1</button>
-        )}
-      </h2>
     </div>
   );
 };
